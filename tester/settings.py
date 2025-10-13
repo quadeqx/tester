dbb576666279ac2ef68c8e44091564c13871826c
brantyyn <brantonmaungu@gmail.com>
2025-10-13 21:37:37 +0300

    Add CSRF trusted origins

--- tester/settings.py
+++ tester/settings.py
@@ -125,3 +125,10 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://qualsudo.com",
+    "https://www.qualsudo.com",
+]
+
